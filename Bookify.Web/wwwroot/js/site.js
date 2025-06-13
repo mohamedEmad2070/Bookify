956a8cc4af9e147a03b0a2fd0f6035d1c254d520
Mohamed Emad <m.emad.ali07@gmail.com>
2025-06-13 17:04:34 +0300

    transport the toggle status code from index to site.js

--- Bookify.Web/wwwroot/js/site.js
+++ Bookify.Web/wwwroot/js/site.js
@@ -170,5 +170,46 @@ $(function () { // <-- Use shorthand for document ready to avoid deprecated jQue
 
         modal.modal('show');
     });
+    //Handle Toggle Status
+    $('body').delegate('.js-toggle-status', 'click', function () {
+        var btn = $(this);
+
+        bootbox.confirm({
+            message: "Are you sure that you need to toggle this item status?",
+            buttons: {
+                confirm: {
+                    label: 'Yes',
+                    className: 'btn-danger'
+                },
+                cancel: {
+                    label: 'No',
+                    className: 'btn-secondary'
+                }
+            },
+            callback: function (result) {
+                if (result) {
+                    $.post({
+                        url: btn.data('url'),
+                        data: {
+                            '__RequestVerificationToken': $('input[name="__RequestVerificationToken"]').val()
+                        },
+                        success: function (lastUpdatedOn) {
+                            var row = btn.parents('tr');
+                            var status = row.find('.js-status');
+                            var newStatus = status.text().trim() === 'Deleted' ? 'Available' : 'Deleted';
+                            status.text(newStatus).toggleClass('badge-light-success badge-light-danger');
+                            row.find('.js-updated-on').html(lastUpdatedOn);
+                            row.addClass('animate__animated animate__flash');
+
+                            showSuccessMessage();
+                        },
+                        error: function () {
+                            showErrorMessage();
+                        }
+                    });
+                }
+            }
+        });
+    });
 
 });
