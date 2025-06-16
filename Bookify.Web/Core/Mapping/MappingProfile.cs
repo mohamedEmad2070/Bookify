39081a0eb01c1549ded2e2998d103e07cc45a55d
Mohamed Emad <m.emad.ali07@gmail.com>
2025-06-16 19:18:46 +0300

    Adding BookFormViiewModel And Modfying MappingProf , Add Create Action and Form View

--- Bookify.Web/Core/Mapping/MappingProfile.cs
+++ Bookify.Web/Core/Mapping/MappingProfile.cs
@@ -1,4 +1,6 @@
-﻿namespace Bookify.Web.Core.Mapping
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace Bookify.Web.Core.Mapping
 {
     public class MappingProfile : Profile
     {
@@ -6,11 +8,27 @@
         {
             //categories
             CreateMap<Category, CategoryViewModel>();
+
             CreateMap<CategoryFormViewModel, Category>().ReverseMap();
+
+            CreateMap<Category, SelectListItem>()
+                .ForMember(dest => dest.Value, opt => opt.MapFrom(src => src.Id))
+                .ForMember(dest => dest.Text, opt => opt.MapFrom(src => src.Name));
+
+
             //Authors
             CreateMap<Author, AuthorViewModel>();
+
             CreateMap<AuthorFormViewModel, Author>().ReverseMap();
 
+            CreateMap<Author, SelectListItem>()
+                .ForMember(dest => dest.Value, opt => opt.MapFrom(src => src.Id))
+                .ForMember(dest => dest.Text, opt => opt.MapFrom(src => src.Name));
+
+
+            //Books
+            CreateMap<BookFormViewModel, Book>();
+               
         }
     }
 }
---
+++ Bookify.Web/Core/ViewModels/BookFormViewModel.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.Extensions.FileProviders;
+
+namespace Bookify.Web.Core.ViewModels
+{
+    public class BookFormViewModel
+    {
+        public int Id { get; set; }
+        [MaxLength(500,ErrorMessage =Errors.MaxLength)]
+        public string Title { get; set; } = null!;
+
+        [Display(Name ="Author")]
+        public string AuthorId { get; set; }
+        public IEnumerable<SelectListItem>? Authors { get; set; }
+
+        [MaxLength(500, ErrorMessage = Errors.MaxLength)]
+        public string Publisher { get; set; } = null!;
+
+        [Display(Name = "Publishing Date")]
+        public DateTime PublishingDate { get; set; }
+        public IFormFile? Image { get; set; }
+        [MaxLength(100, ErrorMessage = Errors.MaxLength)]
+        public string Hall { get; set; } = null!;
+
+        [Display(Name = "Is Available for Rental")]
+        public bool ISAvailableForRental { get; set; }
+
+        public string Description { get; set; } = null!;
+
+        public IList<int> SelectedCategories { get; set; } = new List<int>();
+        public IEnumerable<SelectListItem>? Categories { get; set; }
+    }
+}
